34e8296f92a0deb1bc3f95a5eebb82e5f1561d3e
Gianni Giudice <45078018+GianniGiudice@users.noreply.github.com>
2021-02-15 21:22:01 +0100

    :rocket: Ajout du projet sur Github

---
+++ myatoibase.c
@@ -0,0 +1,79 @@
+//
+// Created by Gianni GIUDICE on 15/02/2021.
+//
+
+#include <string.h>
+
+int convert(char letter)
+{
+    if (letter >= '0' && letter <= '9') {
+        return (int) letter - '0';
+    }
+    return (int)letter - 'A' + 10;
+}
+
+int checkPointerValidity(const char *ptr, const int base)
+{
+    int i;
+
+    // Cas particulier : un - suivi d'un 0 => mauvaise entrée
+    if (ptr[0] == '-' && strlen(ptr) > 1 && ptr[1] == '0') {
+        return 0;
+    }
+    if (base <= 10) {
+        for (i = 0; i < strlen(ptr); i++) {
+            // On vérifie que chaque caractère est cohérent avec la base donnée
+            if (!(ptr[i] >= '0' && ptr[i] < ('0' + base))) {
+                // On fait une exception si c'est le premier caractère => - à la place d'un caractère de la base
+                if (!(i == 0 && ptr[i] == '-')) {
+                    return 0;
+                }
+            }
+        }
+    }
+    else
+    {
+        for (i = 0; i < strlen(ptr); i++) {
+            // On vérifie que chaque caractère est cohérent avec la base donnée
+            if (!((ptr[i] >= '0' && ptr[i] < ('0' + base)) || (ptr[i] >= 'A' && ptr[i] < ('A' + base - 10)))) {
+                // On fait une exception si c'est le premier caractère => - à la place d'un caractère de la base
+                if (!(i == 0 && ptr[i] == '-')) {
+                    return 0;
+                }
+            }
+        }
+    }
+    return 1;
+}
+
+int myatoibase(const char *ptr, const int base)
+{
+    if (!ptr) {
+        return 0;
+    }
+
+    if (!checkPointerValidity(ptr, base)) {
+        return -1;
+    }
+
+    if (base < 2 || base > 37) {
+        return -2;
+    }
+
+    int len = strlen(ptr);
+    int power = 1;
+    int num = 0;
+    int i;
+
+    for (i = len - 1; i >= 0; i--) {
+        if (convert(ptr[i]) >= base) {
+            return -1;
+        }
+
+        num += convert(ptr[i]) * power;
+        power = power * base;
+    }
+
+    return num;
+}
+
